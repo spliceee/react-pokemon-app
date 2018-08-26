ea0b9542660c9bf650daf6bb21b5d7ee18228851
Serhiy Ivaneyko <ivaneyko.serhiy@gmail.com>
2018-08-26 17:58:36 +0300

    Add @material-ui/icons

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 class Navbar extends Component {
   render(){
     return (
-      <div className="Navbar">
-        Navbar
-      </div>
+      <Fragment>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="title" color="inherit">
+              Home
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Fragment>
     );
   }
 }
